321848ae92f26ff02dded95f6bf00fa62d975769
Bratah123 <brandonnguyen3301@gmail.com>
2021-06-02 21:38:42 -0700

    add a command that lets you add new songs easily

--- commands.py
+++ commands.py
@@ -1,3 +1,6 @@
+import json
+import random
+
 import discord
 import youtube_dl as youtube_dl
 from discord.ext import commands
@@ -22,6 +25,8 @@ ffmpeg_options = {
 }
 
 ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+OWNER_CLIENT_ID = "207371595113562124"
 
 
 # Referenced from the Author of discord.py Rapptz
@@ -51,6 +56,9 @@ class Commands(commands.Cog, name="commands"):
 
     def __init__(self, bot):
         self.bot = bot
+        with open('songs.json') as f:
+            data = json.load(f)
+            self.song_data = data
 
     @commands.command(name="playgame", pass_context=True)
     async def play_song(self, ctx):
@@ -67,24 +75,27 @@ class Commands(commands.Cog, name="commands"):
             await ctx.send("Sorry, but I'm already in a voice channel!")
         await asyncio.sleep(3)
 
-        love_decoration_url = "https://www.youtube.com/watch?v=M1PurvnqTjc"
+        rand_idx = random.randint(0, len(self.song_data) - 1)
+        music_to_guess = self.song_data[rand_idx][0]
+        music_url = self.song_data[rand_idx][1]
 
-        player = await YTDLSource.from_url(love_decoration_url, loop=self.bot.loop,
+        player = await YTDLSource.from_url(music_url, loop=self.bot.loop,
                                            stream=True)
         ctx.voice_client.play(player, after=lambda x: print('Player error: %s' % x) if x else None)
-        await ctx.send('Now playing: {}'.format(player.title))
+        await ctx.send("Try guessing the anime by typing in this channel (anyone can try)! You got 3 minutes.")
 
         def check_anime_song(m):
             anime_name = m.content.lower()
-            return anime_name == "bunny girl senpai" and m.channel == ctx.channel
+            return anime_name == music_to_guess.lower() and m.channel == ctx.channel
+
         try:
             user_msg = await self.bot.wait_for('message', check=check_anime_song, timeout=180.0)
-            await ctx.send("Nice, you guessed the correct anime!")
+            await ctx.send("Nice, you guessed the correct anime ({})!".format(music_to_guess.title()))
             ctx.voice_client.stop()
         except Exception as e:
             print(e)
+            await ctx.send("Sorry, you took to long to guess.")
             ctx.voice_client.stop()
-        # timeout for 20 seconds for now, TODO: Check the length of the song
 
     @commands.command(name="leavevc", pass_context=True)
     async def leave_vc(self, ctx):
@@ -94,6 +105,46 @@ class Commands(commands.Cog, name="commands"):
     @commands.command(name="hello", pass_context=True)
     async def handle_hello(self, ctx):
         await ctx.send("Shut up scum.")
+
+    @commands.command(name="suggest_song", pass_context=True)
+    async def suggest_song(self, ctx):
+        args = ctx.message.content.split(" ")
+        if len(args) < 3:
+            await ctx.send("Format: !!suggest_song <youtube_link> <anime name>")
+            return
+
+        youtube_link = args[1]
+        anime_name = " ".join(args[2:])
+
+        if str(ctx.author.id) == OWNER_CLIENT_ID:
+            self.song_data.append([anime_name, youtube_link])
+            with open('songs.json', 'w') as json_file:
+                json_string = json.dumps(self.song_data, indent=4)
+                json_file.write(json_string)
+            print(anime_name)
+            print(youtube_link)
+            await ctx.message.delete()
+            await ctx.send("Successfully added anime song to song database.")
+            return
+        else:
+            await ctx.send("Considering the song suggestion, please wait until Brandon's approval.")
+            if anime_name.startswith("http"):
+                await ctx.send("The correct format is: !!suggest_song <ANIME NAME> <YOUTUBE LINK>")
+                return
+
+            def wait_for_approval(m):
+                return m.content.lower() == "lgtm" and str(m.author.id) == OWNER_CLIENT_ID
+
+            try:
+                user_msg = await self.bot.wait_for('message', check=wait_for_approval, timeout=180.0)
+                self.song_data.append([anime_name, youtube_link])
+                with open('songs.json', 'w') as json_file:
+                    json_string = json.dumps(self.song_data, indent=4)
+                    json_file.write(json_string)
+                await ctx.send("Successfully added anime song to song database.")
+            except Exception as e:
+                print(e)
+                await ctx.send("Ran out of time for approval")
 
 
 def setup(bot):
